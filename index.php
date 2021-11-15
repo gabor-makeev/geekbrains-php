c8cdbe95982f816396e80cc5481ad1d58d773db6
gabormakeev <makeyev@twnty.de>
2021-11-15 22:42:59 +0200

    Lesson-4. Task 3 solved.

--- index.php
+++ index.php
@@ -1,5 +1,9 @@
 <?php
 
+/* 
+  Функция fetchImages() отвечает за сканирование директории с изображениями
+  и создание массива состоящего из адресов ведущих к изображениям
+*/
 function fetchImages() {
   global $images;
   $images = [];
@@ -10,6 +14,11 @@ function fetchImages() {
   }
 }
 
+/* 
+  Тут я проверяю вернул ли image_upload.php (файл, в котором происходит обработка 
+  загрузки нового файла) сообщение об ошибке. В случае если ошибки имеются
+  создаю массив с сообщениями ошибок
+*/
 if ($_REQUEST) {
   $errors = explode(';', $_REQUEST['errors']);
 } else {
@@ -26,24 +35,34 @@ fetchImages();
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>GB</title>
+  <script src='./script.js' defer></script>
 </head>
 <body>
   <div class="wrapper">
     <header class="header">
-      <h1>My images</h1>
+      <h1>Галерея</h1>
     </header>
     <main class="main">
       <div class="gallery">
-      <?php foreach($images as $image_url): ?>
-        <a href=<?= $image_url ?> target="blank" class ="gallery__image-container"><img src=<?= $image_url ?> alt="" class="gallery__image"></a>
+      <!-- Вывод всех изображений из массива images -->
+      <?php foreach($images as $image_idx => $image_url): ?>
+        <!-- 
+          В теге img отсутствует target="blank", так как я реализовал модальное окно из третьего задания
+        -->
+        <img src=<?= $image_url ?> alt=<?= 'image-' . $image_idx ?> class="gallery__image">
       <?php endforeach; ?>
       </div>
       <div class="gallery-image-uploader">
+        <!-- В теге form, в атрибуте action, указан файл, обрабатывающий загрузку изображений -->
         <form action="image_upload.php" method="POST" enctype="multipart/form-data" class="gallery-image-uploader__form">
           <input type="file" name="user_image" required class="gallery-image-uploader__input">
           <input type="submit" value="Загрузить" class="gallery-image-uploader__submit">
         </form>
       </div>
+      <!-- 
+        Тут у меня в случае наличия ошибок после загрузки файла,
+        генерируется блок выводящий сообщения о причине ошибки
+      -->
       <?php if($errors): ?>
       <div class="error-info">
         <h3>Файл, который вы загрузили не соответствует следующим параметрам:</h3>
@@ -57,6 +76,7 @@ fetchImages();
 </body>
 </html>
 
+<!-- Дальше идут просто стили моей Галереи -->
 <style>
   body {
     padding: 0;
@@ -76,7 +96,7 @@ fetchImages();
   }
 
   .header h1 {
-    color: #252B31;
+    color: #12255C;
   }
 
   .gallery {
@@ -84,15 +104,8 @@ fetchImages();
     flex-wrap: wrap;
     justify-content: space-evenly;
     padding: 25px 25px 0 25px;
-    background-color: #C1C8C7;
-  }
-
-  .gallery__image-container {
-    transition: 0.3s;
-  }
-
-  .gallery__image-container:hover {
-    transform: scale(1.025);
+    background-color: #718DE1;
+    border-radius: 5px;
   }
 
   .gallery__image {
@@ -100,6 +113,15 @@ fetchImages();
     height: 134px;
     margin-bottom: 20px;
     object-fit: cover;
+    transition: 0.3s;
+    border-radius: 10px;
+    opacity: 0.9;
+  }
+
+  .gallery__image:hover {
+    transform: scale(1.025);
+    cursor: pointer;
+    opacity: 1;
   }
 
   .gallery-image-uploader {
@@ -116,6 +138,30 @@ fetchImages();
 
   .gallery-image-uploader__input {
     margin-bottom: 10px;
+  }
+
+  .gallery__modal-window {
+    position: absolute;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    background-color: RGB(113, 141, 225, 0.9);
+    padding: 50px;
+  }
+
+  .gallery__modal-window-img {
+    max-width: 70%;
+  }
+
+  .gallery__modal-window-close {
+    position: absolute;
+    right: 20px;
+    top: 20px;
+    cursor: pointer;
   }
 
   .error-info {
